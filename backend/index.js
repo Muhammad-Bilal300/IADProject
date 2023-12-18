c57786bd32e4107531d8e5f4766774f4941003d6
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2023-12-18 13:21:00 -0800

    Change the structure of the project to the properly industry structured code + ports and mongodb URL changed to lastest

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,11 @@ mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to MongoDB");
+
+    
+    const server = app.listen(5000, () => {
+      console.log("Server is running on port 5000!!!");
+    });
   })
   .catch((err) => {
     console.log(err);
